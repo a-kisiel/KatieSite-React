fe11b2105826681d202f0e181ecd4eb10e262f77
Alex Kisiel <akisiel@albany.edu>
2020-12-27 05:00:26 -0500

    hamburger

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,8 +1,8 @@
-import { React, useState} from 'react';
-import MediaQuery, { useMediaQuery } from 'react-responsive';
+import { React, useState } from 'react';
 import { Fade } from 'react-reveal';
+import {Accordion, AccordionDetails, AccordionSummary } from '@material-ui/core';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-import MediaQuery from 'react-responsive'; 
+import { FaBars } from 'react-icons/fa';
 import '../styles/menu.scss';
 
 function getSig() {
@@ -14,24 +14,43 @@ function getSig() {
 }
 
 export default function MenuBar(props) {
+    const [width, setWidth] = useState(window.innerWidth);
+    window.onresize = () => {
+        setWidth(window.innerWidth)
+    }
+    var isOpen = false;
+    let menuComponent = 
+    <div className='menu-bar'>
+        <div id='signature' className='menu-item' href='#top'><img src={getSig()} alt='Katie Kisiel' /></div>
+            <div className='nav-menu'>
+                {/* <AnchorLink className='menu-item' href='#top'>Home</AnchorLink>
+                <p className='menu-item'> | </p> */}
+                <AnchorLink className='menu-item' href='#portfolio'>Portfolio</AnchorLink>
+                <p className='menu-item'> | </p>
+                <AnchorLink className='menu-item' href='#about'>About</AnchorLink>
+            </div>
+    </div>
+    if (width < 620) {
+        menuComponent = 
+        <div className='menu-bar'>
+            <Accordion>
+                <AccordionSummary id='accordion-button' expandIcon={<FaBars id='hamburger-icon'/>}>
+                    <div id='signature' className='menu-item' href='#top'><img src={getSig()} alt='Katie Kisiel' /></div>
+                </AccordionSummary>
+                <AccordionDetails id='accordion-details'>
+                    <div className='nav-menu' style={{margin: 'auto'}}>
+                        <AnchorLink className='menu-item' href='#portfolio'>Portfolio</AnchorLink>
+                        <p className='menu-item'> | </p>
+                        <AnchorLink className='menu-item' href='#about'>About</AnchorLink>
+                    </div>
+                </AccordionDetails>
+            </Accordion>
+        </div>
+    }
     return (
         <div className='menu-wrapper'>
             <Fade>
-                <div className='menu-bar'>
-                    <div id='signature' className='menu-item' href='#top'><img src={getSig()} alt='Katie Kisiel' /></div>
-                        <div className='nav-menu'>
-                            <MediaQuery minDeviceWidth={620}>
-                                <AnchorLink className='menu-item' href='#top'>Home</AnchorLink>
-                                <p className='menu-item'> | </p>
-                                <AnchorLink className='menu-item' href='#portfolio'>Portfolio</AnchorLink>
-                                <p className='menu-item'> | </p>
-                                <AnchorLink className='menu-item' href='#about'>About</AnchorLink>
-                            </MediaQuery>
-                            <MediaQuery maxDeviceWidth={619}>
-
-                            </MediaQuery>
-                        </div>
-                </div>
+                {menuComponent}
             </Fade>
             <div className='clear'></div>
         </div>
