9f0f35a311503ee5fa36042222705938f6efea7b
a-kisiel <60425431+a-kisiel@users.noreply.github.com>
2023-03-05 14:33:15 -0500

    Include aws credentials

---
+++ src/credentials.js
@@ -0,0 +1,13 @@
+var credentials = {
+    // cloudinary_key: '882596136218925',
+    // cloudinary_secret: 'SMxeYlZNzUQ5oEQ_WG_PDcOep0s',
+    ACCESS_ID: 'AKIAUXMII2JOOHN54HPF',
+    ACCESS_KEY: 'iTSAvwBefIyKIDv+4dr3mlP3hDjXQRL71UcLFxnM',
+    BUCKET_NAME: 'katieart',
+    REGION: 'us-east-2',
+    atlas_username: 'akisiel',
+    atlas_password: '9oieIUvf46gGGY2V',
+    // 882596136218925:SMxeYlZNzUQ5oEQ_WG_PDcOep0s@
+}
+
+module.exports = credentials;
